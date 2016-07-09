878d6b5a7763ff178189bc6aef36d9f5c9449f41
Simon Krughoff <simon.krughoff@gmail.com>
2016-07-09 15:55:14 -0700

    Update processFile
    This implements a new FileIsrTask that mangles the file to allow
    passing to ProcessCcdTask.  This also makes ProcessFileTask just
    a very thin wrapper that does little other than set config defaults.
    
    Also move things to bin.src so that the shebang function can be run
    on the command line tools.

---
+++ python/lsst/obs/file/fileisr.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+#
+# LSST Data Management System
+# Copyright 2008-2013 LSST Corporation.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+import numpy
+
+import lsst.afw.image as afwImage
+import lsst.afw.math as afwMath
+import lsst.pex.config as pexConfig
+import lsst.pipe.base as pipeBase
+
+
+__all__ = ["FileIsrTask"]
+
+
+class FileIsrConfig(pexConfig.Config):
+    """Config for FileIsr"""
+    addNoise = pexConfig.Field(dtype=bool, default=False,
+                               doc="Add a flat Poisson noise background to the eimage?")
+    doMask = pexConfig.Field(dtype=bool, default=False, doc="Calculate mask?")
+    gain = pexConfig.Field(dtype=float, default=0.0, doc="Gain (e/ADU) for image")
+    noise = pexConfig.Field(dtype=float, default=0.0, doc="Noise (ADU) in image")
+    saturation = pexConfig.Field(dtype=float, default=65535, doc="Saturation limit")
+    low = pexConfig.Field(dtype=float, default=0.0, doc="Low limit")
+    isBackgroundSubtracted = pexConfig.Field(dtype=bool, default=False,
+                                             doc="Input image is already background subtracted")
+
+
+class FileIsrTask(pipeBase.Task):
+    ConfigClass = FileIsrConfig
+    _DefaultName = "FileIsr"
+
+    @pipeBase.timeMethod
+    def runDataRef(self, sensorRef):
+        """ISR one CCD
+
+        @param sensorRef: sensor-level butler data reference
+        @return pipe_base Struct containing these fields:
+        - exposure: post ISR exposure
+        """
+        self.log.info("FileIsr:  %s" % (sensorRef.dataId))
+
+        # initialize outputs
+        #
+        # We need to be read straight Fits images with no
+        # mask planes or Wcs
+        #
+        postIsrExposure = sensorRef.get("raw", immediate=True)
+        raw_md = sensorRef.get("raw_md", immediate=True)
+
+        postIsrExposure.getMaskedImage().getMask()[:] &= \
+            afwImage.MaskU.getPlaneBitMask(["SAT", "INTRP", "BAD", "EDGE"])
+        if self.config.addNoise:
+            self.addNoise(postIsrExposure)
+
+        self.setVariance(postIsrExposure)
+        if self.config.doMask:
+            self.setMask(postIsrExposure)
+
+        # I'm not sure why this is necessary since the Exposure constructor should set the wcs
+        wcs = afwImage.makeWcs(raw_md)
+        if wcs:
+            postIsrExposure.setWcs(wcs)
+        else:
+            self.log.warn("No WCS found in %s; caveat emptor" % (sensorRef.dataId))
+
+        return pipeBase.Struct(exposure=postIsrExposure)
+
+    def addNoise(self, inputExposure):
+        mi = inputExposure.getMaskedImage()
+        (x, y) = mi.getDimensions()
+        noiseArr = numpy.random.poisson(self.config.noise, size=x*y).reshape(y, x)
+        noiseArr = noiseArr.astype(numpy.float32)
+        noiseImage = afwImage.makeImageFromArray(noiseArr)
+        mi += noiseImage
+        if self.config.isBackgroundSubtracted:
+            mi -= self.config.noise
+
+    def setVariance(self, exposure):
+        mi = exposure.getMaskedImage()
+        image = mi.getImage().getArray()
+        variance = mi.getVariance().getArray()
+        if self.config.isBackgroundSubtracted:
+            bkgdVariance = afwMath.makeStatistics(mi.getImage(), afwMath.VARIANCECLIP).getValue()
+            self.log.info("Setting variance: background variance = %g ADU" % (bkgdVariance))
+        else:
+            self.log.info("Setting variance: noise=%g ADU" % (self.config.noise))
+            bkgdVariance = self.config.noise**2
+
+        variance[:] = bkgdVariance
+
+        if self.config.gain > 0.0:
+            self.log.info("Setting variance: gain=%g e/ADU" % (self.config.gain))
+            variance[:] += image/self.config.gain
+
+    def setMask(self, exposure):
+        mi = exposure.getMaskedImage()
+        image = mi.getImage().getArray()
+        mask = mi.getMask().getArray()
+        isLow = image < self.config.low
+        isSat = image > self.config.saturation
+        self.log.info("Masking %d low and %d saturated pixels" % (isLow.sum(), isSat.sum()))
+        mask += numpy.where(isLow, afwImage.MaskU.getPlaneBitMask("BAD"), 0)
+        mask += numpy.where(isSat, afwImage.MaskU.getPlaneBitMask("SAT"), 0)
+
--- python/lsst/obs/file/processFile.py
+++ python/lsst/obs/file/processFile.py
@@ -20,32 +20,26 @@
 # the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
-import numpy
-import os
-from lsst.pipe.tasks.processImage import ProcessImageTask
-import lsst.afw.image as afwImage
-import lsst.afw.math as afwMath
+from lsst.pipe.tasks.processCcd import ProcessCcdTask
+from lsst.pipe.base.argumentParser import ArgumentParser
 import lsst.afw.table as afwTable
-from lsst.pex.config import Field
-import lsst.pex.exceptions as pexExcept
-import lsst.pipe.base as pipeBase
 
-from .argumentParser import FileArgumentParser
+from .fileisr import FileIsrTask
 
-class ProcessFileConfig(ProcessImageTask.ConfigClass):
+
+class ProcessFileConfig(ProcessCcdTask.ConfigClass):
     """Config for ProcessFile"""
-    doCalibrate=Field(dtype=bool, default=True, doc="Perform calibration?")
-    doVariance=Field(dtype=bool, default=False, doc="Calculate variance?")
-    doMask=Field(dtype=bool, default=False, doc="Calculate mask?")
-    gain=Field(dtype=float, default=0.0, doc="Gain (e/ADU) for image")
-    noise=Field(dtype=float, default=1.0, doc="Noise (ADU) in image")
-    saturation=Field(dtype=float, default=65535, doc="Saturation limit")
-    low=Field(dtype=float, default=0.0, doc="Low limit")
-    isBackgroundSubtracted=Field(dtype=bool, default=False, doc="Input image is already background subtracted")
-    
-class ProcessFileTask(ProcessImageTask):
+
+    def setDefaults(self):
+        ProcessCcdTask.ConfigClass.setDefaults(self)
+        self.isr.retarget(FileIsrTask)
+        self.calibrate.doAstrometry = False
+        self.calibrate.doPhotoCal = False
+
+
+class ProcessFileTask(ProcessCcdTask):
     """Process a CCD
-    
+
     Available steps include:
     - calibrate
     - detect sources
@@ -54,104 +48,15 @@ class ProcessFileTask(ProcessImageTask):
     ConfigClass = ProcessFileConfig
     _DefaultName = "processFile"
 
-    def __init__(self, **kwargs):
-        ProcessImageTask.__init__(self, **kwargs)
-
     def makeIdFactory(self, sensorRef):
         expBits = sensorRef.get("ccdExposureId_bits")
         expId = long(sensorRef.get("ccdExposureId"))
-        return afwTable.IdFactory.makeSource(expId, 64 - expBits)        
+        return afwTable.IdFactory.makeSource(expId, 64 - expBits)
 
     @classmethod
     def _makeArgumentParser(cls):
         """Create an argument parser
         """
-        parser = FileArgumentParser(name=cls._DefaultName)
-        parser.add_id_argument(name="--id", datasetType="calexp",
-                               help="data ID, e.g. --id calexp=XXX")
-
+        parser = ArgumentParser(name=cls._DefaultName)
+        parser.add_id_argument("--id", "raw", "data ID, e.g. visit=1 raft=2,2 sensor=1,1 snap=0")
         return parser
-
-    @pipeBase.timeMethod
-    def run(self, sensorRef):
-        """Process one CCD
-        
-        @param sensorRef: sensor-level butler data reference
-        @return pipe_base Struct containing these fields:
-        - exposure: calibrated exposure (calexp): as computed if config.doCalibrate,
-            else as upersisted and updated if config.doDetection, else None
-        - calib: object returned by calibration process if config.doCalibrate, else None
-        - sources: detected source if config.doPhotometry, else None
-        """
-        self.log.info("Processing %s" % (sensorRef.dataId))
-
-        # initialize outputs
-        #
-        # Be careful.  If we've already run processFile.py, then there's a copy of the
-        # input file written into the output directory, and the butler will find it before
-        # it finds the original.  We also need to be read straight Fits images with no
-        # mask planes or Wcs
-        #
-        inputFile = sensorRef.get("calexp_filename")[0]
-        fileDir, fileName = os.path.split(inputFile)
-        originalFile = os.path.join(fileDir, "_parent", fileName)
-        if os.path.exists(originalFile):
-            inputFile = originalFile
-
-        try:
-            postIsrExposure = afwImage.ExposureF(inputFile)
-        except pexExcept.LsstCppException, e:
-            etype = e.args[0].getType()
-            if etype == "lsst::afw::fits::FitsError *":
-                import lsst.daf.base as dafBase
-                md = dafBase.PropertyList()
-                mi = afwImage.MaskedImageF(inputFile, md)
-
-                postIsrExposure = afwImage.makeExposure(mi)
-
-                wcs = afwImage.makeWcs(md)
-                if wcs:
-                    postIsrExposure.setWcs(wcs)
-                else:
-                    self.log.warn("No WCS found in %s; caveat emptor" % (sensorRef.dataId))
-            else:
-                raise
-
-        postIsrExposure.getMaskedImage().getMask()[:] &= \
-            afwImage.MaskU.getPlaneBitMask(["SAT", "INTRP", "BAD", "EDGE"])
-        if self.config.doVariance:
-            self.setVariance(postIsrExposure)
-        if self.config.doMask:
-            self.setMask(postIsrExposure)
-        
-        # delegate the work to ProcessImageTask
-        result = self.process(sensorRef, postIsrExposure)
-        return result
-
-    def setVariance(self, exposure):
-        mi = exposure.getMaskedImage()
-        image = mi.getImage().getArray()
-        variance = mi.getVariance().getArray()
-        if self.config.isBackgroundSubtracted:
-            bkgdVariance = afwMath.makeStatistics(mi.getImage(), afwMath.VARIANCECLIP).getValue()
-            self.log.info("Setting variance: background variance = %g ADU" % (bkgdVariance))
-        else:
-            self.log.info("Setting variance: noise=%g ADU" % (self.config.noise))
-            bkgdVariance = self.config.noise**2
-
-        variance[:] = bkgdVariance
-
-        if self.config.gain > 0.0:
-            self.log.info("Setting variance: gain=%g e/ADU" % (self.config.gain))
-            variance[:] += image/self.config.gain
-
-    def setMask(self, exposure):
-        mi = exposure.getMaskedImage()
-        image = mi.getImage().getArray()
-        mask = mi.getMask().getArray()
-        isLow = image < self.config.low
-        isSat = image > self.config.saturation
-        self.log.info("Masking %d low and %d saturated pixels" % (isLow.sum(), isSat.sum()))
-        mask += numpy.where(isLow, afwImage.MaskU.getPlaneBitMask("BAD"), 0)
-        mask += numpy.where(isSat, afwImage.MaskU.getPlaneBitMask("SAT"), 0)
-

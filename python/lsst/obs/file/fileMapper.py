a85f31703d6ff8734b547e371520b5c5257c2cba
Michael Wood-Vasey <wmwv@pitt.edu>
2017-03-15 08:50:22 -0700

    Use makefileRawVisitInfo implementation.

--- python/lsst/obs/file/fileMapper.py
+++ python/lsst/obs/file/fileMapper.py
@@ -6,11 +6,15 @@ from lsst.obs.base import CameraMapper
 import lsst.afw.cameraGeom as afwCg
 import lsst.afw.image.utils as afwImageUtils
 import lsst.pex.policy as pexPolicy
-from filecam import FileCam
+from .filecam import FileCam
+
+from .makeFileRawVisitInfo import MakeFileRawVisitInfo
 
 class FileMapper(CameraMapper):
     """Provides abstract-physical mapping for data found in the filesystem"""
     packageName = 'obs_file'
+
+    MakeRawVisitInfoClass = MakeFileRawVisitInfo
     
     def __init__(self, **kwargs):
         policyFile = pexPolicy.DefaultPolicyFile("obs_file", "FileMapper.paf", "policy")
